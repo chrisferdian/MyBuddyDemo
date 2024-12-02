fbeed570cf0a9eb07e6e09f8c38223cc2cf4d91c
Chris Ferdian <chris.ferdian@gmail.com>
2024-12-02 20:44:38 +0700

    Initial commit

---
+++ MyBuddyDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBuddyDemo
+//
+//  Created by Indo Teknologi Utama on 02/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBuddyDemo/MyBuddyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBuddyDemoApp.swift
+//  MyBuddyDemo
+//
+//  Created by Indo Teknologi Utama on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBuddyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
